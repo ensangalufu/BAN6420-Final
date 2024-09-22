9864ac466e889a19691dc7f00b73aad812bfa4e3
Ernest Nsangalufu <ernest.nsangalufu@gmail.com>
2024-09-22 18:24:42 +0300

    Fix proxy params

--- app.py
+++ app.py
@@ -74,5 +74,11 @@ def export_data():
     
     return f"Data exported to {file_name}"
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
+
 if __name__ == '__main__':
     app.run(debug=True)
